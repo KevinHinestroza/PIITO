3bc3ba549bd5936d02828af73bbb51bcce67e45d
KevinHinestroza <hinestrozakev@gmail.com>
2026-01-03 18:26:03 -0500

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,15 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api',
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: "http://localhost:3000/api",
 });
 
 export default api;
