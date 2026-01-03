9f2eb5541459bc0d91530c2760676ba4888888e5
KevinHinestroza <hinestrozakev@gmail.com>
2026-01-03 18:17:08 -0500

    Update scoringService.js

--- backend/services/scoringService.js
+++ backend/services/scoringService.js
@@ -1,1 +1,15 @@
+export function calculateAffinityScore(person) {
+  let score = 0;
+
+  if (person.politicalInterest) score += person.politicalInterest * 0.3;
+  if (person.voteProbability) score += person.voteProbability * 0.4;
+  if (person.mobilizationProbability) score += person.mobilizationProbability * 0.3;
+
+  return Math.min(score, 1);
+}
+
+export function calculateVolatility(person) {
+  if (!person.volatilityIndex) return 0.5;
+  return person.volatilityIndex;
+}
 
